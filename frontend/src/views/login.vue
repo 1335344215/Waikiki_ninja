9432db938cf1d1b767fb521bc82e2dfd2b97c751
1335 <1335344215@qq>
2021-09-28 15:32:17 +0800

    改端口为520

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -12,7 +12,6 @@
        <p>建议登录后修改备注为自己QQ,并加Q群:699935945</p>
        <a style="" href="https://qr.dingtalk.com/action/joingroup?code=v1,k1,DCC1kAGM9FkkBNPxJuuAshLK//V04kTOlmc4iJBBz2k=&_dt_no_comment=1&origin=11" target="_blank" id="waikiki">钉钉群直达</a>
       <a style="" href="https://jq.qq.com/?_wv=1027&k=XbkLgMIN" target="_blank" id="waikiki">QQ群直达</a>
-
        <b>安全起见，WSCK可以在CK登录后录入，期限半永久。</b>
       </div>
 
